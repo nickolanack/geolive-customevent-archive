4e6f5042b927a4ac695de065c56faba8466c33d7
nickolanack <nickblackwell82@gmail.com>
2015-11-17 12:37:43 -0800

    unarchive on empty date

--- custom.plugin.Attributes.onUpdateRecord.php
+++ custom.plugin.Attributes.onUpdateRecord.php
@@ -10,16 +10,23 @@ if (! empty($eventArgs)) {
     $tableMetadata = AttributesTable::GetMetadata('markerAttributes');
     $values = AttributesRecord::GetFields($marker->getId(), $marker->getType(), 'sessionDate', $tableMetadata);
     
-    $date = strtotime($values['sessionDate']);
+
+    $sessionDate=$values['sessionDate'];
+
+    $date = strtotime($sessionDate);
     $limit = time() - (30 * 24 * 3600);
-    if ($date > $limit) {
+
+
+    // Unarchive items without data, or within date range
+
+    if (empty($sessionDate) || $date > $limit) {
         file_put_contents(__DIR__ . DS . '.custom.log', 
-            'detect revive (' . $marker->getId() . ': ' . $values['sessionDate'] . ')' . "\n\n", FILE_APPEND);
+            'detect revive (' . $marker->getId() . ': ' . $sessionDate . ')' . "\n\n", FILE_APPEND);
         Core::Emit('custom.revive', $eventArgs);
     } else {
         
         file_put_contents(__DIR__ . DS . '.custom.log', 
-            'detect expire (' . $marker->getId() . ': ' . $values['sessionDate'] . ')' . "\n\n", FILE_APPEND);
+            'detect expire (' . $marker->getId() . ': ' . $sessionDate . ')' . "\n\n", FILE_APPEND);
         
         Core::Emit('custom.expire', $eventArgs);
     }
